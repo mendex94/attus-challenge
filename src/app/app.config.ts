788657cee09a1f45cb12033df659a0cab1aaac2a
Felipe Mendes <lfelipecmendes@icloud.com>
2025-03-13 13:14:52 -0300

    config: add `provideTanStackQuery` on `app.config.ts`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import {
+  provideTanStackQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
 
 import { routes } from './app.routes';
 
@@ -9,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideTanStackQuery(new QueryClient()),
   ],
 };
