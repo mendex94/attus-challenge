9f8566fb38e6caad51288b5f058bad753db6e54e
Felipe Mendes <lfelipecmendes@icloud.com>
2025-03-14 13:33:15 -0300

    feat: add routes for list products, create and edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/products-page/products-page.component').then(
+        (m) => m.ProductsPageComponent
+      ),
+  },
+  {
+    path: 'products/create',
+    loadComponent: () =>
+      import('./pages/product-create/product-create.component').then(
+        (m) => m.ProductCreateComponent
+      ),
+  },
+  {
+    path: 'products/edit/:id',
+    loadComponent: () =>
+      import('./pages/product-update/product-update.component').then(
+        (m) => m.ProductUpdateComponent
+      ),
+  },
+];
