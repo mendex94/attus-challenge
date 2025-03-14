fb6c4125de0029cb47e960a7e2a6de17cb5ea45c
Felipe Mendes <lfelipecmendes@icloud.com>
2025-03-14 13:27:25 -0300

    feat(ui): add header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
+
+@Component({
+  selector: 'app-header',
+  imports: [DividerModule],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {}
